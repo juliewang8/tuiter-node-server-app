136f52941022b8cecb58642af4281272e2494037
Julie Wang <juliewang@Julies-MacBook-Pro.local>
2023-04-03 01:57:14 -0400

    changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import UserController from './controllers/users/users-controller.js';
 import TuitsController
   from "./controllers/tuits/tuits-controller.js";
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 TuitsController(app);
 HelloController(app); 
