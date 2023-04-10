edcbf122368aa129db4375d8856b925a75b47926
Julie Wang <juliewang@Julies-MacBook-Pro.local>
2023-04-09 23:42:32 -0400

    changing order of mongoose connection

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ import UserController from './controllers/users/users-controller.js';
 import TuitsController
   from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,6 +16,3 @@ TuitsController(app);
 HelloController(app); 
 UserController(app);
 app.listen(process.env.PORT || 4000);
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
- || 'mongodb://127.0.0.1:27017/tuiter'
-mongoose.connect(CONNECTION_STRING);
